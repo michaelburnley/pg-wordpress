8051685ae8632ac75884550026f018b3b1aef780
Michael Burnley <micheal.burnley@v1.co>
2018-10-26 18:53:50 -0700

    pre-design review

--- index.js
+++ index.js
@@ -2,21 +2,32 @@
 
 // Element Variables
 const blocks = document.getElementsByClassName('brand-block');
-const brands_block = document.getElementsByClassName('brands')[0];
+const brands_block = document.getElementById('brand_tile_wrapper');
+const brand_large = document.getElementById('brand-large-image');
 const close_button = document.getElementById('close');
 
 
 // Click Handlers
-const openBrandView = () => {
-    brands_block.style.display = 'none';
+const openBrandView = (e) => {
+    brand_large.children[0].src = e.target.dataset.gallery;
+    brands_block.style.opacity = '0';
+    brands_block.style.visibility = 'hidden';
+    brand_large.style.visibility = 'visible';
+    brand_large.style.opacity = '1';
 }
 
 const closeBrandView = () => {
-    
+    brands_block.style.visibility = 'visible';
+    brand_large.style.visibility = 'hidden';
+    brand_large.style.opacity = '0';
+    brands_block.style.opacity = '1';
+    brand_large.children[0].src = "";
 }
 
 // Adding event listeners
-for (let block of blocks) {
+for (var block of blocks) {
+    console.log("test");
+    console.log(block);
     block.addEventListener('click', openBrandView);
 }
 
